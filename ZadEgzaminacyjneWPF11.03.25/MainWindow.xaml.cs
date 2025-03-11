9f9a0ff103bff070b9494a5e2379ab2234efc93d
Sadovsky33 <kacpersadik@gmail.com>
2025-03-11 11:00:42 +0100

    ShowMessage

--- ZadEgzaminacyjneWPF11.03.25/MainWindow.xaml.cs
+++ ZadEgzaminacyjneWPF11.03.25/MainWindow.xaml.cs
@@ -15,11 +15,71 @@ namespace ZadEgzaminacyjneWPF11._03._25
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// Kacper Sadecki
+    /// 
+   
     public partial class MainWindow : Window
     {
+        
+       
         public MainWindow()
         {
             InitializeComponent();
+            string male = "qwertyuiopasdfghjklzxcvbnm";
+            string wielkie = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            int cyfry = 1234567890;
+            string specjalne = "!@#$%^&*()_+-";
+
+            for (int i = 0; i < male.Length; i++)
+            {
+
+            }
+           
+        }
+
+        private void Imie_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Nazwisko_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Stanowisko_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void IleZnakow_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void MaleWielkieLitery_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cyfry_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZnakiSpecjalne_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerujBttn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZatwierdzBttn_Click(object sender, RoutedEventArgs e)
+        {
+        
+            MessageBox.Show(("Dane Pracownika:") + Imie.Text + (" ") + Nazwisko.Text + (" ") + Stanowisko.Text +(" ") + ("Hasło:"));
         }
     }
 }
