802b36d3548a4e90a222f92fe90e33acfe1a4852
Sadovsky33 <kacpersadik@gmail.com>
2025-03-11 10:17:30 +0100

    Dodaj pliki projektów.

---
+++ ZadEgzaminacyjneWPF11.03.25/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ZadEgzaminacyjneWPF11._03._25
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
